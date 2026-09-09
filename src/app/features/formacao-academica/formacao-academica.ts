37dfc7ecce1769ca437f26b0e4693a9452d7f693
Ayla Pessoa <aylamargarida21@gmail.com>
2026-09-09 18:25:31 -0300

    feat: estilizacao completa do header e telas conforme prototipo figma

---
+++ src/app/features/formacao-academica/formacao-academica.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formacao-academica',
+  standalone: true,
+  imports: [],
+  templateUrl: './formacao-academica.html',
+  styleUrl: './formacao-academica.css'
+})
+export class FormacaoAcademica {}
---
+++ src/app/features/objetivo-profissional/objetivo-profissional.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-objetivo-profissional',
+  standalone: true,
+  imports: [],
+  templateUrl: './objetivo-profissional.html',
+  styleUrl: './objetivo-profissional.css'
+})
+export class ObjetivoProfissional {}
